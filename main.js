d56f364c06a434f76dcdbd490937c3b8e6b876d0
Fermin <ferminlozanorodriguez@gmail.com>
2023-08-15 15:37:28 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// Initialize WebGPU context
+const canvas = document.querySelector("canvas");
+if (!navigator.gpu) {
+  throw new Error("WebGPU not supported on this browser.");
+}
+
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+  throw new Error("No appropriate GPUAdapter found.");
+}
+
+const device = await adapter.requestDevice();
+
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
+
